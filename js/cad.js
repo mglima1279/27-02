99fc4acf6858f31f9c088c96d370ade5551b868f
Miguel Lima <mglima1279@gmail.com>
2026-03-06 07:54:41 -0300

    cep working

--- js/cad.js
+++ js/cad.js
@@ -2,51 +2,54 @@ import { app, getFirestore, setDoc, doc } from "./firebase/config.js"
 
 const db = getFirestore()
 
-const cepInput = document.getElementById("cep");
-const addressItems = document.querySelectorAll(".hiddenItem");
-let formExpanded = false;
+const cepInput = document.getElementById("cep")
+const addressItems = document.querySelectorAll(".hiddenItem")
+const addresInput = document.querySelectorAll(".addresInput")
+let formExpanded = false
 
 cepInput.addEventListener("input", () => {
     const val = cepInput.value
 
     if (val.length == 8 && !isNaN(val)) {
-        expandForm(val);
+        expandForm(val)
     } else if (val.length < 8 && formExpanded) {
-        collapseForm();
+        collapseForm()
     }
-});
+})
 
 function expandForm(cep) {
     fetch(wrapUrl(cep))
         .then(res => res.json())
         .then(data => {
             if (data.erro) {
-                alert("CEP não encontrado.");
-                collapseForm();
-                return;
+                alert("CEP não encontrado.")
+                collapseForm()
+                return
             }
 
-            addressItems[0].value = data.logradouro;
+            addresInput[0].value = data.logradouro
 
             addressItems.forEach(el => {
                 el.classList.remove("hiddenItem")
-            });
-            formExpanded = true;
+            })
+            formExpanded = true
         })
         .catch(err => {
-            console.error("Erro na busca:", err);
-            collapseForm();
-        });
+            console.error("Erro na busca:", err)
+            collapseForm()
+        })
 }
 
 function collapseForm() {
+    let n = 0
     addressItems.forEach(el => {
-        el.classList.add("hiddenItem");
-        el.value = "";
-    });
-    formExpanded = false;
+        el.classList.add("hiddenItem")
+        addresInput[n].value = ""
+        n++
+    })
+    formExpanded = false
 }
 
 function wrapUrl(cep) {
-    return `https://viacep.com.br/ws/${cep}/json/`;
+    return `https://viacep.com.br/ws/${cep}/json/`
 }
